691fbd16136b6c7b8e52c2c5750f3a94338b0616
UndersunIO <alex.atc.digital@gmail.com>
2022-01-20 19:04:47 +0100

    Fin 19h07

--- src/index.js
+++ src/index.js
@@ -1,17 +1,85 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { useState } from "react";
+import ReactDOM from "react-dom";
+import axios from "axios";
+import "./index.css";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+function App() {
+  
+  const [books, setBooks] = useState(null);
+  const [characters, setCharacters] = useState(null);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+  const fetchBooks = async () => {
+    const response = await axios.get(
+      "https://www.anapioficeandfire.com/api/books?pageSize=50"
+    );
+    setBooks(response.data);
+  };
+ 
+  const fetchCharacters = async () => {
+    const response2 = await axios.get(
+      "https://www.anapioficeandfire.com/api/characters?pageSize=50"
+    );
+    setCharacters(response2.data);
+  };
+
+  return (
+    <div className="App">
+      <h1>Game of Thrones</h1>
+      <h2>Jouons avec l'API</h2>
+
+     
+      <div>
+        <button className="fetch-button" onClick={fetchCharacters}> Personnage </button>
+        <br />
+      </div>
+      <div>
+        <button className="fetch-button space" onClick={fetchBooks}> Livres </button>
+        <br />
+      </div>
+
+
+      <div className="frames">
+        {books &&
+          books.map((book, index) => {
+            const CleanDate = new Date(book.released).toDateString();
+              return (
+                <div className="book" key={index}>
+                  <h3>Livres n°{index + 1}</h3>
+                  <h2>{book.name}</h2>
+
+                  <div className="details">
+                    <p><strong>{book.numberOfPages} pages</strong></p> 
+                    <p><strong>Publier par : </strong> {book.publisher} </p>
+                    <p><strong>Sortie le :</strong> {CleanDate}</p>
+                  </div>
+                </div>
+              );
+          })}
+      </div>
+
+      <div className="frames">
+        {characters &&
+          characters.map((character, index) => {
+            return (
+              <div className="book" key={index}>
+                <h3>Personnage n°{index + 1}</h3>
+                <h2>{character.name}</h2>
+              
+                <div className="details">
+                  <p><strong>Surnom:   </strong>{character.aliases} </p>
+                  <p><strong>Nee : </strong>{character.born}</p> 
+                  <p><strong>Mort : </strong>{character.died}</p> 
+                  <p><strong>Genre : </strong>{character.gender}</p> 
+                  <p><strong>Joue par: </strong>{character.playedBy} </p>
+                  <p><strong>Culture : </strong>{character.culture}</p>  
+                </div>
+              </div>
+            );
+          })}
+      </div>
+    </div>
+  );
+}
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
